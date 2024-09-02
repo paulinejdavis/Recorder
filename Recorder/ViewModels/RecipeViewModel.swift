f4a96dc6b9eb08f40f773dae7fbf01d162b6995b
pauline <pauljdavi.t21@btinternet.com>
2024-09-02 11:08:42 +0100

    Added images and formatting for recipe images

--- Recorder/ViewModels/RecipeViewModel.swift
+++ Recorder/ViewModels/RecipeViewModel.swift
@@ -32,45 +32,69 @@ struct RecipeViewModel: RecipeViewModelProtocol {
             
         ),
         Recipe(
-            title: "No-barrel Jerk Chicken2",
-            description: "A spicy flavourful chicken recipe without the traditional barrel cooking.",
+            title: "Saltfish Fritters",
+            description: "A spicy flavourful chicken recipe with the traditional homemade cooking.",
             image: {
-                let image = UIImage(named: "jerk_chicken")
+                let image = UIImage(named: "saltfish_fritters")
                 if  image == nil {
                     print("image not found")
                 }
                 return image
             }(),
-            ingredients: ["A spicy flavourful chicken recipe without the traditional barrel cooking."]
+            ingredients: ["A spicy flavourful saltfish recipe with the traditional Carib cooking."]
             
         ),
         Recipe(
-            title: "No-barrel Jerk Chicken",
-            description: "A spicy flavourful chicken recipe without the traditional barrel cooking.",
+            title: "Patties",
+            description: "A spicy pattie with lashes of flavour.",
             image: {
-                let image = UIImage(named: "jerk_chicken")
+                let image = UIImage(named: "patties")
                 if  image == nil {
                     print("image not found")
                 }
                 return image
             }(),
-            ingredients: ["A spicy flavourful chicken recipe without the traditional barrel cooking."]
+            ingredients: ["A spicy blah blah recipe without the traditional Carib cooking."]
             
         ),
         Recipe(
-            title: "No-barrel Jerk Chicken2",
-            description: "A spicy flavourful chicken recipe without the traditional barrel cooking.",
+            title: "Banana Bread",
+            description: "A flavourful spiced bun recipe without the traditional Caribbean cooking.",
             image: {
-                let image = UIImage(named: "jerk_chicken")
+                let image = UIImage(named: "banana_bread")
                 if  image == nil {
                     print("image not found")
                 }
                 return image
             }(),
-            ingredients: ["A spicy flavourful chicken recipe without the traditional barrel cooking."]
+            ingredients: ["A spicy bun recipe without the traditional Caribbean cooking."]
             
-        )
+        ),
+        Recipe(
+            title: "Fried Plaintain",
+            description: "The simple things are the best.",
+            image: {
+                let image = UIImage(named: "fried_plaintain")
+                if  image == nil {
+                    print("image not found")
+                }
+                return image
+            }(),
+            ingredients: ["A spicy flavourful recipe with traditional Carib cooking vibrations."]
+            
+        ),
+        Recipe(
+            title: "Stew Chicken",
+            description: "The simple things are the best.",
+            image: {
+                let image = UIImage(named: "stew_chicken")
+                if  image == nil {
+                    print("image not found")
+                }
+                return image
+            }(),
+            ingredients: ["A spicy flavourful chicken recipe with the traditional Carib cooking."]
     
     
-    ]
+    )]
 }
